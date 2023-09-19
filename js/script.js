aa9b723ee6e30be38dfb70e1da7ccd18ba7a4343
Rmls <109774335+Remelens@users.noreply.github.com>
2023-09-19 23:45:14 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,11 @@ document.addEventListener('copy', function (event) {
 function closemsg(){
     document.getElementsByClassName("message")[0].style.display='none';
 }
+document.addEventListener('visibilitychange', function() {
+    var title=document.getElementsByTagName('title');
+    if(document.visibilityState=='hidden'){
+        title[0].innerHTML= "页面崩溃啦qwq";
+    }else{
+	    title[0].innerHTML="Remelens的个人主页！";
+    }
+});
