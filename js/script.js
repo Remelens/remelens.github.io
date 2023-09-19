7c092dd89217b6d20f9a209b0143db34d4d8af80
Rmls <109774335+Remelens@users.noreply.github.com>
2023-09-19 23:43:19 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('copy', function (event) {
     let clipboardData = event.clipboardData || window.clipboardData;
     if (!clipboardData) { return; }
     let text = window.getSelection().toString();
-    let add = "\n----------\n除非特别声明, 本文遵循CC-BY-SA-4.0协议，转载时请附上作者和原文链接。\n作者: HelloOSMe\n原文: "+window.location.href;
+    let add = "\n----------\n除非特别声明, 本文遵循CC-BY-SA-4.0协议，转载时请附上作者和原文链接。\n作者: Remelens\n原文: "+window.location.href;
     if (text) {
         event.preventDefault();
         clipboardData.setData('text/plain', text + add);
